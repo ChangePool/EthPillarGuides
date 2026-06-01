809590948d5e914f377fd11a14f5aba9e52c36c1
ChangePool <doc@coincashew.io>
2026-06-01 08:39:07 -0700

    Create Top-level sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,7 +176,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/CCSocialCard.png',
+      image: 'img/EthPillarSocialCard.png',
       navbar: {
         title: 'EthPillar Guides',
         logo: {
